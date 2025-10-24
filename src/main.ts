b71ddb40cf0f02957a7099b955690d0c62932162
Esther1703 <esther@kinetic.com>
2025-10-24 13:33:27 -0500

    initial push Cosmos files

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import './style.css'
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+import 'photoswipe/style.css'; // Vite will load CSS
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: '#stellar-gallery',
+  children: 'a',
+  counter: false,
+  pswpModule: () => import('photoswipe')
+});
+lightbox.init();
+
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//   </div>
+// `
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
